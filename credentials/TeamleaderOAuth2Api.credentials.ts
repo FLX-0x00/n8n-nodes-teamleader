30bef852b1bdb6b0a98d33db3cf22ec090c7e2df
Paul Werther <pw@evait.de>
2024-11-14 00:20:23 +0100

    Update documentation URL for Teamleader OAuth2 API

--- credentials/TeamleaderOAuth2Api.credentials.ts
+++ credentials/TeamleaderOAuth2Api.credentials.ts
@@ -7,7 +7,7 @@ export class TeamleaderOAuth2Api implements ICredentialType {
 
 	displayName = 'Teamleader OAuth2 API';
 
-	documentationUrl = 'teamleader';
+	documentationUrl: 'https://developer.teamleader.eu/';
 
     icon: Icon = 'file:teamleader.svg';
 
